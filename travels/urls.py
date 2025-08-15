1e07b2f0468e8e6c610cfbcf42d2e8ca77afa305
Payourse <payourse@Payourses-MacBook-Pro.local>
2025-08-15 14:16:03 +0100

    new commit

--- travels/urls.py
+++ travels/urls.py
@@ -9,7 +9,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path("api/travel-plans/", TravelPlanCreateView.as_view(), name="travel-plans"),
+    path("travel-plans/", TravelPlanCreateView.as_view(), name="travel-plans"),
     path("available-rides/", AvailableRidesListView.as_view(), name="available-rides"),
     path("book-ride/", BookingCreateView.as_view(), name="book-ride"),
     path("my-bookings/", MyBookingsListView.as_view(), name="my-bookings"),
